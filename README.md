aa4d3e0448a9a85fc25969b4e1eab67006b1c8ef
Cosmodash <theo.github@bedafamily.com>
2021-06-12 23:41:31 -0700

    rework arguments

--- README.md
+++ README.md
@@ -4,13 +4,16 @@ This action creates a zip file for distribution.
 
 ## Inputs
 
-### `files`
+### `config`
 
-**Required** A newline-separated list of files to read.
+The path to the configuration JSON file; must be in this format:
 
-### `output-file`
+`outputPath`: the path to the output zip file
 
-The path to the output zip file to be written. Default is `./release.zip`.
+`entries`: An array of objects that follow the specified schema:
+    `type`: Must be one of `"glob"`, `"file"`, or `"directory"`
+    `source`: The path to the source glob, file, or directory
+    `destination`: The path to put the selected file(s) into, inside the zip file
 
 ## Outputs
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 const fs = require("fs");
+const archiver = require("archiver");
 const core = require("@actions/core");
 const github = require("@actions/github");
-const files = core.getInput("files");
+var config = JSON.parse(fs.readFileSync(core.getInput("config")));
+const output = fs.createWriteStream(config.outputPath);
+const archive = archiver("zip", {
+    zlib: {
+        level: 9
+    }
+});
+output.on("close", function() {
+    console.log("Wrote " + archive.pointer() + " total bytes");
+});
+archive.pipe(output);
+config.entries.forEach(file => {
+    switch (file.type) {
+        case "glob":
+            archive.glob(file.source, { cwd: file.destination });
+            break;
+        case "file":
+            archive.file(file.source, { name: file.destination });
+            break;
+        case "directory":
+            archive.directory(file.source, file.destination);
+            break;
+    }
+});
+core.setOutput("path", config.outputPath);
